26eae975df54e5af1329c33d9c25982ae30d725d
flyingeek <68456+flyingeek@users.noreply.github.com>
2021-01-15 17:35:20 +0000

    added timezones to airports.json

--- scripts/makeAirports.js
+++ scripts/makeAirports.js
@@ -3,10 +3,12 @@ import fs from 'fs';
 import got from 'got';
 
 const airportsPath = "./data/airports.json";
+const timezonesPath = "./data/timezones.json";
 const gistURL = "https://github.com/mborsetti/airportsdata/raw/main/airportsdata/airports.csv";
 
 let results = "";
-
+let timezones = {};
+let timezonesIndex = 0;
 const processRow = (row) => {
     const [icao, iata, name, city, subd, country, elevation, lat, lon, tz] = row.data;
     if (icao && icao !== "icao" && iata && city && tz) {
@@ -16,17 +18,35 @@ const processRow = (row) => {
         } else if ((iata === 'EWR') || (country === "US" && city === "New York")) {
             taxCountry = "NY";
         }
-        results += `${iata}:${taxCountry}`;
+        let tzRef;
+        if (tz in timezones) {
+            tzRef = timezones[tz];
+        } else {
+            tzRef = timezonesIndex.toString(36).padStart(2, "0");
+            timezones[tz] = tzRef;
+            timezonesIndex++;
+        }
+        results += `${iata}:${taxCountry}${tzRef}`;
     }
 };
 
 const save = () => {
+ const reversedTz = Object.assign({}, ...Object.entries(timezones).map(([a,b]) => ({ [b]: a })));
+
     fs.writeFile(airportsPath, JSON.stringify(results), (err) => {
         if (err) {
             throw err;
         } else {
-            console.log(`Saved ${results.length / 6} airports! in ${airportsPath}`);
+            console.log(`Saved ${results.length / 8} airports! in ${airportsPath}`);
         }
+    });
+    fs.writeFile(timezonesPath, JSON.stringify(reversedTz),
+        (err) => {
+            if (err) {
+                throw err;
+            }else{
+                console.log(`Saved ${Object.keys(reversedTz).length} timezones! in ${timezonesPath}`);
+            }
     });
 };
 
