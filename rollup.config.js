27be2258406d5919fdc34553947265f73f3649a4
Eric Delord <68456+flyingeek@users.noreply.github.com>
2021-01-25 01:44:10 +0100

    better quality screenshots

--- rollup.config.js
+++ rollup.config.js
@@ -32,8 +32,8 @@ const U = {
     'CONF_ABRILFATFACE_WOFF2': '../fonts/abril-fatface-v12-latin-ext_latin-regular.woff2', /* relative to public/css/bundle.css */
     'CONF_ABRILFATFACE_WOFF': '../fonts/abril-fatface-v12-latin-ext_latin-regular.woff',
     'CONF_DATASET_URLS': Array.from(new Set(DATASET.map(o => o.url))).join(';'),
-    'CONF_FM_EXAMPLE_IMG': './img/fm_exemple.jpg',
-    'CONF_S_EXAMPLE_IMG': './img/s_exemple.jpg'
+    'CONF_FM_EXAMPLE_IMG': './img/fm_exemple2.jpg',
+    'CONF_S_EXAMPLE_IMG': './img/s_exemple2.jpg'
 };
 function serve() {
     let server;
