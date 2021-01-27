flyingeek/flytax

commit 599edc8959d580024555aa027179f22c8fab9e03

removed preloading of taxData, reload on external waiting if no user data

src/components/TaxYearSelect.svelte
@@ -1,7 +1,6 @@
 <script>
     import {taxYear, DATASET, taxData, swDismiss} from "../stores";
     const change = () => {
-        $taxData; //force preload
         $swDismiss = false; // show popup update again
     };
 </script>

rollup.config.js
@@ -14,6 +14,7 @@ import watchAssets from 'rollup-plugin-watch-assets';
 import html from '@open-wc/rollup-plugin-html';
 import {DATASET} from './src/stores';
 const Mustache = require('mustache');
+import fs from 'fs';
 
 const production = !process.env.ROLLUP_WATCH;
 const relPath = (url) => url.replace('./', './public/'); // public path for a local url
@@ -139,7 +140,10 @@ export default [{
 
         // Watch the `public` directory and refresh the
         // browser on changes when not in production
-        !production && livereload({watch: 'public', port:35728}),
+        !production && livereload({watch: 'public', port:35728, https: (process.env.SERVE === 'start2') ? {
+            key: fs.readFileSync('localhost-key.pem'),
+            cert: fs.readFileSync('localhost-cert.pem')
+        } : null}),
 
         // If we're building for production (npm run build
         // instead of npm run dev), minify

src/components/SWUpdate.svelte
@@ -1,11 +1,24 @@
 <script context="module">
-    import {writable} from 'svelte/store';
-    import {swDismiss, wb} from '../stores';
+    import {writable, get} from 'svelte/store';
+    import {swDismiss, wb, paySlips, ep5} from '../stores';
     export const swUpdated = writable(false);
     export const swRegistration = writable();
-    export const showSkipWaitingPrompt = () => {
-        swUpdated.set(true);
-        swDismiss.set(false);
+    export const showSkipWaitingPrompt = (isExternal) => {
+        if (isExternal && 'serviceWorker' in navigator && Object.keys(get(ep5)).length === 1 && Object.keys(get(paySlips)).length === 1) {
+            navigator.serviceWorker.addEventListener('controllerchange', () => {
+                window.location.reload();
+            });
+            try {
+                get(swRegistration).waiting.postMessage({type: 'SKIP_WAITING'});
+                console.debug('external waiting was received and no user data found => SKIP_WAITING => reload');
+            } catch {
+                console.debug('external waiting was received, no registration waiting => reload ');
+                window.location.reload();
+            }
+        } else {
+            swUpdated.set(true);
+            swDismiss.set(false);
+        }
     };
 </script>
 <script>
@@ -24,13 +37,13 @@
             });
             //This does not fire when Workbox mark event as isExternal
             $wb.addEventListener('controlling', () => {
-                console.debug('controlling')
+                console.debug('controlling ')
                 if (refreshing) return;
                 refreshing = true;
                 window.location.reload();
             });
             installLabel = "En cours...";
-            $wb.messageSkipWaiting();
+            $swRegistration.waiting.postMessage({type: 'SKIP_WAITING'});
         }else{ /* update probably done in another tab */
             console.debug('no reg.waiting')
             window.location.reload();

src/main.js
@@ -44,7 +44,7 @@ try {
             }else{
                 //console.log('Updated Service worker activated');
             }
-            console.debug('Updated Service worker activated', event.isUpdate, event.isExternal)
+            console.debug('Updated Service worker activated ', event.isUpdate, event.isExternal)
         });
         workbox.addEventListener('waiting', (event) => {
             //console.log(`A new service worker has installed, but it can't activate` +
@@ -55,14 +55,14 @@ try {
                 });
                 workbox.messageSkipWaiting();
             }else{
-                console.debug('Updated Service worker waiting ', event.isUpdate, event.isExternal);
-                showSkipWaitingPrompt();
+                console.debug('Updated Service worker waiting', event.isUpdate, event.isExternal, event.wasWaitingBeforeRegister);
+                showSkipWaitingPrompt(event.isExternal);
             }
         });
         workbox.register().then(reg => {
             if (appError && reg) reg.unregister();
-            if (reg) swRegistration.set(reg);
-            console.debug(reg);
+            swRegistration.set(reg);
+            //console.debug(reg);
         }, console.error);
     }
 }

src/stores.js
@@ -1,4 +1,4 @@
-import { writable, readable, derived} from 'svelte/store';
+import { writable, readable, derived } from 'svelte/store';
 import {iso2FR, mergeRots} from './parsers/ep5Parser';
 
 export const BASES = [
@@ -65,6 +65,7 @@ const patchLog = () => {
 }
 
 export const log = patchLog();
+
 export const ep5 = resettable({type: "ep5"});
 export const paySlips = resettable({type: "pay"});
 export const nuiteesInput = resettable();
@@ -128,7 +129,8 @@ export const wb = writable();
 let swLastUpdateDate = new Date();
 export const checkSWUpdate = () => {
     if ('serviceWorker' in navigator) {
-        if ((new Date() - swLastUpdateDate) > 900000) { /* 15mn */
+        const timeout = ('process.env.NODE_ENV' !== '"development"') ? 1800000 /* 30 mn */ : 2000;
+        if ((new Date() - swLastUpdateDate) > timeout) {
             navigator.serviceWorker.getRegistration().then(reg => {
                 if (reg) {
                     if (!reg.waiting) reg.update();
@@ -141,8 +143,7 @@ export const checkSWUpdate = () => {
 }
 export const handleVisibilityChange = () =>{
     if (document && document.visibilityState && document.visibilityState === 'visible') {
-        //console.debug('handleVisibilityChange: visible');
-        window.setTimeout(checkSWUpdate, 2000);
+        checkSWUpdate();
     }
 }
 

commit 7b1512457302b2bcfdb2f65ac1ef9ee1f566af79

bug fix:  599edc8 broke site

src/components/TaxYearSelect.svelte
@@ -1,6 +1,7 @@
 <script>
     import {taxYear, DATASET, taxData, swDismiss} from "../stores";
     const change = () => {
+        $taxData; //force preload
         $swDismiss = false; // show popup update again
     };
 </script>